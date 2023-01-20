4e5341a35f641e9c74859ae2d4e28d353b2c6846
Codezz <112660193+Codezz-ops@users.noreply.github.com>
2023-01-20 04:07:11 -0500

    Update main.cpp

--- Half-Life/main.cpp
+++ Half-Life/main.cpp
@@ -3,7 +3,7 @@
 
 void Halflife()
 {   
-    float N, N_o, halflife, val, ans;
+    float N, N_o, halflife, val, t;
     std::cout << "Enter ending value: ";
     std::cin >> N;
     std::cout << "Enter starting value: ";
@@ -11,8 +11,8 @@ void Halflife()
     std::cout << "Enter halflife: ";
     std::cin >> halflife;
     val = N/N_o;
-    ans = halflife * log(val)/log(.5);
-    std::cout << "It will take " << ans << " cents, years, weeks, days, mins, secs, millsecs... amount of time to decay this isotope";
+    t = halflife * log(val)/log(.5);
+    std::cout << "It will take " << t << " cents, years, weeks, days, mins, secs, millsecs... amount of time to decay this isotope";
 }
 
 int main()
